cce332249e4c5b89890f911325b9292202c81bc3
wbrunovieira <wbrunovieira77@gmail.com>
2020-09-04 08:11:04 +0100

    learned static

---
+++ StaticExercice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticExercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var ceulcius = TempConverter.FahrenheitToCelsius(68);
+
+            Console.WriteLine($"celcius: {ceulcius}");
+
+
+            var fahrenheit = TempConverter.CeulsiusToFahrenheit(20);
+
+            Console.WriteLine($"fahrenheit: {fahrenheit}");
+        }
+    }
+}
---
+++ StaticExercice/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace StaticExercice
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double faherenheit)
+        {
+            var result = (faherenheit - 32) / 1.8;
+
+            return result;
+        }
+
+        public static double CeulsiusToFahrenheit(double celcius)
+
+        {
+            var result = (celcius * 9) / 5 + 32;
+
+            return result;
+
+        }
+             
+         
+    }
+}
